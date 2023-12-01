4dd0c7e173f667012c9828ee231144a47c5c7c67
gbotin <geoffrey.botin@gmail.com>
2023-12-01 18:42:48 +0100

    Finishing base keymap

---
+++ keyboards/lily58/keymaps/fractory/rules.mk
@@ -0,0 +1,12 @@
+BOOTMAGIC_ENABLE = no
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE= yes
+REPEAT_KEY_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+SRC +=  ./keymaps/fractory/lib/layer_state_reader.c \
+        ./keymaps/fractory/lib/keylogger.c \
+        ./lib/logo_reader.c \
+        ./lib/timelogger.c \
